7b9a71641ba463c207cb9756ab74802a140ab247
kennyshah <67391456+kennyshah@users.noreply.github.com>
2020-06-25 02:50:29 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Units_Input = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Output_Label = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(45, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Units :"
+        '
+        'Units_Input
+        '
+        Me.Units_Input.Location = New System.Drawing.Point(87, 31)
+        Me.Units_Input.Name = "Units_Input"
+        Me.Units_Input.Size = New System.Drawing.Size(79, 20)
+        Me.Units_Input.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.Units_Input, "Enter Units Here")
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TextBox2.Location = New System.Drawing.Point(48, 68)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(180, 124)
+        Me.TextBox2.TabIndex = 6
+        Me.ToolTip1.SetToolTip(Me.TextBox2, "Units Entered Will Be Displayed Here")
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 247)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&Enter"
+        Me.ToolTip1.SetToolTip(Me.Button1, "Press Enter to Insert units")
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(101, 247)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 33)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "&Reset"
+        Me.ToolTip1.SetToolTip(Me.Button2, "To Clear The Form")
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button3.Location = New System.Drawing.Point(182, 247)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 33)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "&Exit"
+        Me.ToolTip1.SetToolTip(Me.Button3, "To Exit The Application")
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(174, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Day :"
+        Me.ToolTip1.SetToolTip(Me.Label2, "Day Of The Week For Which The Unit Has Been Entered")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(206, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(14, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "1"
+        '
+        'Output_Label
+        '
+        Me.Output_Label.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Output_Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Output_Label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Output_Label.Location = New System.Drawing.Point(48, 205)
+        Me.Output_Label.Name = "Output_Label"
+        Me.Output_Label.Size = New System.Drawing.Size(180, 26)
+        Me.Output_Label.TabIndex = 5
+        Me.Output_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.Output_Label, "Units Average Will Be Displayed Here")
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.CancelButton = Me.Button3
+        Me.ClientSize = New System.Drawing.Size(269, 296)
+        Me.Controls.Add(Me.Output_Label)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Units_Input)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = " Units Averager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Units_Input As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Output_Label As Label
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ Form1.vb
@@ -0,0 +1,95 @@
+﻿
+' Author : Kenny Shah
+' Date   : May 28, 2020
+' Description : 
+'   A simple VB.net form which allows a user to enter the 
+'   number of units shipped by them in 7 days and in turn will 
+'   help them to know how many average units have been shipped by them.
+
+Option Strict On
+Public Class Form1
+
+    '---Variable Declarations---
+
+    Dim Input As Integer        'Used for stroring user input.
+
+    Dim Sum As Integer = 0      'Initializing sum to 0 for addition of the units user will enter.
+
+    Dim Counter As Integer = 0  'Counter variable used for showing day as well as in average calculation.
+
+    Dim Day As Integer = 1      'For storing the number of days.
+
+    Dim Average As Double       'For storing the calculated average.
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        '------MAIN PROGRAM LOGIC------
+
+        'Cheking if the input field is empty if it is then prompting the user that field should not be empty.
+
+        If String.IsNullOrEmpty(Units_Input.Text) Then
+            MessageBox.Show("The Units Field Should Not Be Empty.", "Alert!")
+            Units_Input.Focus()
+
+            'Checking if the user enetered value is integer or not.
+
+        ElseIf Integer.TryParse(Units_Input.Text, Input) = False Then
+
+            MessageBox.Show("Units Value Should Be An Integer.", "Alert!")
+            Units_Input.Focus()
+            Units_Input.SelectAll()
+
+            'To check wheter the user entered value is in the range 0-5000.
+
+        ElseIf Input < 0 Or Input > 5000 Then
+
+            MessageBox.Show("Units should be in range of 0-5000.", "Alert!")
+            Units_Input.Focus()
+            Units_Input.SelectAll()
+
+            'If the input is properly validated then perfroming further calculations.
+
+        Else
+
+            TextBox2.Text &= CStr(Input) & vbCrLf  'This line will append the entered values in the texbox below which will act as an displaybox.
+            Units_Input.Clear()                    'This line will clear the input field once the value is inserted in the program as well as in the displaybox.
+            Sum += Input                      'It will sum the inputs the user has entered.
+            Counter += 1            'This counter is used here to let the program know that after 7 values it has to stop the input and then show the output.
+            Day += 1                    'This line is used to increment the day which gives us the numbers of day i.e. Day 1, Day 2, Day 3.... etc.
+
+            If Counter = 7 Then
+                Day = 7            'This block of code will prevent the counter from exceeding the number of days, i.e. at Day 7 it will stop and won't get any further.
+            End If
+
+            Label3.Text = CStr(Day)  'This label is used to display the Day number and by using the Cstr function the integer will be converted to string.
+            Units_Input.Focus()
+        End If
+
+
+
+        Average = Sum / 7       'Used to calculate the average after getting the sum
+
+        If Counter = 7 Then             'This codeblock will be executed when the counter reaches 7 and as a result it will lock the user input as well as the enter button.
+            Units_Input.Enabled = False
+            Button1.Enabled = False
+            Output_Label.Text = CStr(" Average per day : " & Math.Round(Average, 2)) 'Calculated average shown in the label with 2 decimal places.
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Output_Label.Text = ""
+        TextBox2.Clear()           'Once the reset button is clicked it will clear the output label, displaybox and will activate the inputbox and enter button.
+        Label3.Text = CStr(1)
+        Units_Input.Enabled = True
+        Button1.Enabled = True
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()                                                                      'By clicking the exit button the application stops working
+    End Sub
+
+
+End Class
